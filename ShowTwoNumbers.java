0f1d7efa475a070e1be41239e89d96b6a583e751
Duy Cong Vu <duycongvu@MacBook-Pro-cua-Duy.local>
2020-02-24 03:53:09 +0700

    Second commit

--- ShowTwoNumbers.java
+++ ShowTwoNumbers.java
@@ -4,14 +4,22 @@ class ShowTwoNumbers {
     public static void main(String[] args) {
         String num1, num2;
         String notification = "You've just entered ";
+        double num_1, num_2;
 
-        num1 = JOptionPane.showInputDialog(null, "Please enter the first number: ", "Input the first number", JOptionPane.INFORMATION_MESSAGE);
-        notification += num1 + " and ";
+        try {
+            num1 = JOptionPane.showInputDialog(null, "Please enter the first number: ", "Input the first number", JOptionPane.INFORMATION_MESSAGE);
+            notification += num1 + " and ";
+            num_1 = Double.parseDouble(num1);
 
-        num2 = JOptionPane.showInputDialog(null, "Please enter the second number: ", "Input the second number", JOptionPane.INFORMATION_MESSAGE);
-        notification += num2;
-
-        JOptionPane.showMessageDialog(null, notification, "Show two numbers", JOptionPane.INFORMATION_MESSAGE);
-        System.exit(0);
+            num2 = JOptionPane.showInputDialog(null, "Please enter the second number: ", "Input the second number", JOptionPane.INFORMATION_MESSAGE);
+            notification += num2;
+            num_2 = Double.parseDouble(num2);
+            
+            JOptionPane.showMessageDialog(null, notification, "Show two numbers", JOptionPane.INFORMATION_MESSAGE);
+            System.exit(0);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Invalid input!", "Error", JOptionPane.INFORMATION_MESSAGE);
+            System.exit(0);
+        }  
     }
 }
